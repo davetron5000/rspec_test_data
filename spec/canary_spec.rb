c0babf065d88dea71468be723ad76ffd2f07938e
Dave Copeland <davec@naildrivin5.com>
2022-11-14 09:52:40 -0500

    bump to 1.0

--- spec/canary_spec.rb
+++ spec/canary_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 RSpec.describe "testing that testing works" do
-  it "does" do
+  it "does, although there are no tests for this gem. See README" do
     expect(true).to eq(true)
   end
 end
